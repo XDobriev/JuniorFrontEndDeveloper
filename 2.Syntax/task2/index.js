f82d2d5a2e5c5771b25e3afa0a1541e5f5c85246
XDobriev <XDobriev@yandex.ru>
2023-02-27 14:17:03 +0300

    feat: complete 2 lesson JS

---
+++ 2.Syntax/task2/index.js
@@ -0,0 +1,14 @@
+// "JavaScript код с созданием переменных"
+/* Автор:
+Добриев Хамзат Юсупович */
+const myName = 'Хамза';
+const mySurname = 'Добриев';
+let myFavoriteDrink = 'Молочный коктейль';
+let myFavoriteAnimal = 'Черепашка';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
---
+++ 2.Syntax/task3/car.js
@@ -0,0 +1,8 @@
+const carName = 'ВАЗ2114';
+let maxCarSpeed = '200 км/ч';
+let carOwner = 'Добриев Хамзат';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
