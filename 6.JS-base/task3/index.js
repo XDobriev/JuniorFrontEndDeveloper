c4ebd37a409c91451f2247bfdab1cf065b01afae
XDobriev <XDobriev@yandex.ru>
2023-05-25 20:47:50 +0300

    complete task 6.3 from course Minin Junior Frontend Developer

---
+++ 6.JS-base/task3/index.js
@@ -0,0 +1,15 @@
+if (confirm('JavaScript появился в 1995 году?') === true) {
+    alert('Верно');
+} else {
+   alert('Неверно! JS появился в 1995 году')
+};
+if (confirm('Спецификация JavaScript называется ECMAScript?') === true) {
+    alert('Верно');
+} else {
+   alert('Неверно! Спецификация называется ECMAScript')
+};
+if (confirm('JavaScript был создан за 1 месяц?') === true) {
+    alert('Верно');
+} else {
+   alert('Неверно! JavaScript был создан за 1 месяц')
+};
