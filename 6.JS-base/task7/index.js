15b20067b8a5e96f9889d0575293ad8fc77e0d5f
XDobriev <XDobriev@yandex.ru>
2023-05-29 15:26:56 +0300

    complete task 6.7

--- 6.JS-base/task7/index.js
+++ 6.JS-base/task7/index.js
@@ -18,23 +18,32 @@ clientSpentForAllTime (например, “hello”), то выведите в 
 пользователю идти дальше к подсчету скидки. Используйте if else, чтобы реализовать
 данную логику */
 
-const clientName = 'Игорь';
-let clientSpentForAllTime = 110;
-let clientSpentToday = 25;
+const clientName = prompt('Введите имя клиента');
+let clientSpentToday = prompt ('Сколько клиент потратил сегодня?');
+clientSpentToday = Number(clientSpentToday);
+let clientSpentForAllTime = prompt('Сколько клиент потратил за все время?');
+clientSpentForAllTime = Number(clientSpentForAllTime)
 
 let discount = 0;
 
-if (clientSpentForAllTime >= 100 && clientSpentForAllTime < 300) {
-    discount = 10;
-} else if (clientSpentForAllTime >= 300 && clientSpentForAllTime < 500) {
-    discount = 20;
-} else if (clientSpentForAllTime >= 500) {
-    discount = 30;
+console.log(!clientSpentToday);
+
+if (!clientSpentToday || !clientSpentForAllTime) {
+    alert('Сумма, которую клиент потратил за все время и которую потратил сегодня, должна быть числом! Перезагрузите страницу, чтобы повторить попытку.')
+} else {
+        if (clientSpentForAllTime >= 100 && clientSpentForAllTime < 300) {
+        discount = 10;
+        } else if (clientSpentForAllTime >= 300 && clientSpentForAllTime < 500) {
+            discount = 20;
+        } else if (clientSpentForAllTime >= 500) {
+            discount = 30;
+        }
+
+        alert(`Вам предоставляется скидка в ${discount}%!`);
+
+        clientSpentToday = clientSpentToday - (clientSpentToday * discount / 100);
+        clientSpentForAllTime += clientSpentToday;
+
+        alert(`К оплате ${clientSpentToday}$. За все время в нашем ресторане вы потратили ${clientSpentForAllTime}.`);
 }
 
-alert(`Вам предоставляется скидка в ${discount}%!`);
-
-clientSpentToday = clientSpentToday - (clientSpentToday * discount / 100);
-clientSpentForAllTime += clientSpentToday;
-
-alert(`К оплате ${clientSpentToday}$. За все время в нашем ресторане вы потратили ${clientSpentForAllTime}.`)
