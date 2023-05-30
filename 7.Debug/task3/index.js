4014f5599563c9594725726d733a137c0c3496b4
XDobriev <XDobriev@yandex.ru>
2023-05-30 22:46:12 +0300

    complete task 7.3 - debug

--- 7.Debug/task3/index.js
+++ 7.Debug/task3/index.js
@@ -18,3 +18,13 @@ totalJuniorDevelopersSalary);
 Исправьте все ошибки, чтобы программа работала правильно. Также выполните
 следующие условия: баг должен быть исправлен при помощи console.log либо debugger, в
 коде должен присутствовать минимум один console.log либо debugger. */
+
+const salaryOfJuniorDeveloper = 500;
+const numberOfJuniorDevelopers = 3;
+let taxPercentage = 13;
+let totalJuniorDevelopersSalary = 0;
+for (let i = 0; i < numberOfJuniorDevelopers; i += 1) {
+    const salaryWithTax = salaryOfJuniorDeveloper - (salaryOfJuniorDeveloper * taxPercentage / 100);
+    totalJuniorDevelopersSalary += salaryWithTax;
+}
+console.log('totalJuniorDevelopersSalary', totalJuniorDevelopersSalary);
