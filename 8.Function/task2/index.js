c3788d9abb74653d901d7f1f3112b76e065695cd
XDobriev <XDobriev@yandex.ru>
2023-05-31 09:15:08 +0300

    complete task 8.2 - function (some of them i still dont understand. Need more practise.

--- 8.Function/task2/index.js
+++ 8.Function/task2/index.js
@@ -14,3 +14,22 @@ getSumOfNumbers принимает в себя 2 параметра: number и t
 2. number = 10, type = ‘even’. Возвращает 30.
 3. number = 10, type = ‘’. Возвращает 55. */
 
+
+const getSumOfNumbers = (number, type = 'odd') => {
+    let sum = 0;
+    for (let i = 0; i <= number; i++) {
+        const isEven = i % 2 === 0;
+        const isOdd = !isEven;
+        if (!type) {
+            sum +=i;
+        } else if (isEven && type === 'even') {
+            sum +=i;
+        } else if (isOdd && type === 'odd') {
+            sum +=i;
+        }
+    }
+    return sum;
+    console.log(sum);
+}
+const result = getSumOfNumbers(10, '');
+console.log('result', result);
